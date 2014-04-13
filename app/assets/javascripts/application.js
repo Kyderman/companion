442c0573e5c3f8834b8c9cd385a06505b45ed53f
Kyle Selman <kyle@kyleselman.co.uk>
2014-04-13 19:10:31 +0100

    js2coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,19 +18,5 @@
 //= require_tree .
 
 
-$("#users a.add_fields").
-  data("association-insertion-position", 'before').
-  data("association-insertion-node", 'this');
 
-$('#users').bind('insertion-callback',
-     function() {
-         $(".teams-user-fields a.add_fields").
-             data("association-insertion-position", 'before').
-             data("association-insertion-node", 'this');
-         $('.teams-user-fields').bind('insertion-callback',
-              function() {
-                $(this).children("#user_from_list").remove();
-                $(this).children("a.add_fields").hide();
-              });
-     });
 
