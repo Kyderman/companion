e3b3b6035e048595a776aaabbb3e8f7e04efceb3
Kyle Selman <kyle@kyleselman.co.uk>
2014-04-13 19:36:33 +0100

    w

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,13 +34,5 @@ $('#teams').bind('insertion-callback',
               });
      });
      
-     $("#users a.add_fields").data("association-insertion-position", "before").data "association-insertion-node", "this"
-$("#users").bind "insertion-callback", ->
-  $(".teams-user-fields a.add_fields").data("association-insertion-position", "before").data "association-insertion-node", "this"
-  $(".teams-user-fields").bind "insertion-callback", ->
-    $(this).children("#user_from_list").remove()
-    $(this).children("a.add_fields").hide()
-    return
 
-  return
 
